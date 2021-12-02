0e7c22d3ac86967be0ec08848fd49007e6a6e9f2
Newton Karanu <karanunewton4@gmail.com>
2021-12-02 08:51:18 +0300

    created tasks and task components

---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "@/components/Task";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array
+  },
+  components: {
+    Task,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
