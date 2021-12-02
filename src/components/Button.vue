4dd0c722318728c28dbb9c5eb5f8032ca1ee02f5
Newton Karanu <karanunewton4@gmail.com>
2021-12-02 08:19:04 +0300

    created button component and attached it to header

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="onClick()" :style="{background: color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+//  import { MDBBtn } from "mdb-vue-ui-kit";
+
+    export default {
+        name: 'Button',
+        components: {
+            // MDBBtn
+        },
+        props: {
+            text: {
+                type: String, 
+            },
+            color: String,
+        },
+        methods: {
+          onClick() {
+                console.log('click');
+            }
+        }
+    }
+</script>
