fcdbf489a63a9b578c8fd30d0151f0423ddaa0a2
Newton Karanu <karanunewton4@gmail.com>
2021-12-02 09:19:26 +0300

    added delete directive --WIP

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="container">
-    <Header title = "Task Tracker" />
-    <Tasks :tasks="tasks" />
+    <Header title="Task Tracker"/>
+    <Tasks @delete-task="deleteTask" :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+
 export default {
   name: "App",
   components: {
     Header,
     Tasks
   },
-  data () {
+  data() {
     return {
       tasks: []
     }
   },
-  created () {
+  methods: {
+    deleteTask(id) {
+      if (confirm('Are you sure you want to delete ' + this.tasks.find(x => x.id === id).text + '?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    }
+  },
+  created() {
     this.tasks = [
       {
         id: 1,
@@ -45,14 +53,17 @@ export default {
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
+
 body {
   font-family: "Poppins", sans-serif;
 }
+
 .container {
   max-width: 500px;
   margin: 30px auto;
@@ -62,6 +73,7 @@ body {
   padding: 30px;
   border-radius: 5px;
 }
+
 .btn {
   display: inline-block;
   background: #000;
@@ -75,12 +87,15 @@ body {
   font-size: 15px;
   font-family: inherit;
 }
+
 .btn:focus {
   outline: none;
 }
+
 .btn:active {
   transform: scale(0.98);
 }
+
 .btn-block {
   display: block;
   width: 100%;
